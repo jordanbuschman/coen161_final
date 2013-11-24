3d41c42e9f439292363703d701b1cdc2feef1601
Jordn Buschman <jordan.buschman@me.com>
2013-11-23 18:28:55 -0800

    Added more tables

--- init.sql
+++ init.sql
@@ -1,15 +1,24 @@
 CREATE DATABASE IF NOT EXISTS kidzcamp;
 USE kidzcamp;
 
-DROP TABLE IF EXISTS user;
+DROP TABLE IF EXISTS user, item;
 
-CREATE TABLE IF NOT EXISTS user (
-	id int NOT NULL AUTO_INCREMENT,
+CREATE TABLE user (
+	id INT NOT NULL AUTO_INCREMENT,
 	username VARCHAR(32) NOT NULL,
 	password VARCHAR(32) NOT NULL,
 	firstName VARCHAR(32) NOT NULL,
 	lastName VARCHAR(32),
-	created TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+	created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	PRIMARY KEY (id));
+
+CREATE TABLE item (
+	id INT NOT NULL AUTO_INCREMENT,
+	location VARCHAR(32) NOT NULL,
+	name VARCHAR(32) NOT NULL,
+	price FLOAT(24, 2) NOT NULL,
 	PRIMARY KEY (id));
 
 INSERT INTO user (username, password, firstName, lastName) VALUES ("jbob69", "potato", "Jim", "Bob");
+
+INSERT INTO item (location, name, price) VALUES ("images/potato.jpg", "potato", 29.99);
