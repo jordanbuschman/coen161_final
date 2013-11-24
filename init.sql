218b304088f6220830099b77d950a592f660cfb4
Jordn Buschman <jordan.buschman@me.com>
2013-11-23 17:01:20 -0800

    Added timestamp collumn to user table

--- init.sql
+++ init.sql
@@ -3,6 +3,13 @@ USE kidzcamp;
 
 DROP TABLE IF EXISTS user;
 
-CREATE TABLE IF NOT EXISTS user (id int NOT NULL AUTO_INCREMENT, username VARCHAR(32) NOT NULL, password VARCHAR(32) NOT NULL, firstName VARCHAR(32) NOT NULL, lastName VARCHAR(32), PRIMARY KEY (id));
+CREATE TABLE IF NOT EXISTS user (
+	id int NOT NULL AUTO_INCREMENT,
+	username VARCHAR(32) NOT NULL,
+	password VARCHAR(32) NOT NULL,
+	firstName VARCHAR(32) NOT NULL,
+	lastName VARCHAR(32),
+	created TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+	PRIMARY KEY (id));
 
 INSERT INTO user (username, password, firstName, lastName) VALUES ("jbob69", "potato", "Jim", "Bob");
