706156eaa119de4e8b99090f11c0f1be7f6c69c0
Jordn Buschman <jordan.buschman@me.com>
2013-11-25 14:24:48 -0800

    Working on login and connection to mysql server

---
+++ check.php
@@ -0,0 +1,15 @@
+<?php
+	$host = "localhost";
+	$sql_username = "";
+	$sql_password = "";
+	$db = "kidzcamp";
+	$tbl = "user";
+
+	mysql_connect($host, $sql_username, $sql_password) or die ("Cannot connect to SQL server.");
+	mysql_select_db("$db") or die ("Cannot select kidzcamp database. (have you ran init.sql?)");
+	
+	$username = $_POST['username'];
+	$password = $_POST['password'];
+	
+	echo "<p>USERNAME: $username, PASSWORD: $password</p>";
+?>
---
+++ login.php
@@ -0,0 +1,17 @@
+<?
+?>
+<form name="login" method="post" action="check.php">
+	<table>
+		<tr>
+			<th>Username</th>
+			<td><input name="username" type="text" id="username"></td>
+		</tr>
+		<tr>
+			<th>Password</th>
+			<td><input name="password" type="text" id="password"></td>
+		</tr>
+		<tr colspan=2">
+			<td><input type="submit" name="Submit" value="Login"></input>
+		</tr>
+	</table>
+</form>
