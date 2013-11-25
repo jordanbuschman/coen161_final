c82f7cac9d9a3ebbaa7e968fcc9a856ad9199df9
Jordan <jordan.buschman@me.com>
2013-11-25 15:03:05 -0800

    Changed input field and included js file

--- login.php
+++ login.php
@@ -1,3 +1,6 @@
+<script src="http://code.jquery.com/jquery-1.7.2.js"></script>
+<script src="javascript.js"></script>
+
 <form name="login" method="post" action="check.php">
 	<table>
 		<tr>
@@ -6,7 +9,7 @@
 		</tr>
 		<tr>
 			<th>Password</th>
-			<td><input name="password" type="text" id="password"></td>
+			<td><input name="password" type="password" id="password"></td>
 		</tr>
 		<tr colspan=2">
 			<td><input type="submit" name="Submit" value="Login"></input>
