9d8b10f7346ed130acc646e5713c3649d2a0d9a0
Jordan <jordan.buschman@me.com>
2013-11-26 18:12:36 -0800

    Added createUser.php

---
+++ createUser.php
@@ -0,0 +1,18 @@
+<script src="http://code.jquery.com/jquery-1.7.2.js"></script>
+<script src="javascript.js"></script>
+
+<form name="login" method="post" action="check.php">
+	<table>
+		<tr>
+			<th>Username</th>
+			<td><input name="username" type="text" id="newUsername"></td>
+		</tr>
+		<tr>
+			<th>Password</th>
+			<td><input name="password" type="password" id="newPassword"></td>
+		</tr>
+		<tr>
+			<td><input type="submit" name="Submit" value="Create" id="create" disabled="disabled"></input>
+		</tr>
+	</table>
+</form>
