306d2e7a8e9ade6c7eee1aef8b03e4299e443db9
Jordan <jordan.buschman@me.com>
2013-11-26 18:00:17 -0800

    Creating an account (Password checking works, username checking doesn't)

--- javascript.js
+++ javascript.js
@@ -1,2 +1,19 @@
-$(function() {
+	var validUser = false;
+	var validPassword = false;
+
+$('document').ready(function() {
+	$('#newUsername').change(function() { //Check if desired username is valid or not with ajax
+		var username = $(this).val();
+	});
+	$('#newPassword').keyup(function() { //Check if desired password is between 8-12 characters with at least 1 capital letter and 1 number
+		var password = $(this).val();
+		var pattern = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,16}$/ 
+		if (password.match(pattern)) validPassword = true;
+		else validPassword = false;
+
+		if (validPassword && validUser){
+			$('#create').removeAttr('disabled');
+		}
+		else $('#create').attr('disabled', 'disabled');
+	});
 });
