567b6a09368a224277d627bf7b25cd6e353575b4
Jordan <jordan.buschman@me.com>
2013-11-27 02:31:33 -0800

    All login and logout features word (Sans several fields)

--- addUser.php
+++ addUser.php
@@ -1,6 +1,7 @@
 <?php
 	if (isset($_SESSION["user"]))
 		header("location: index.php");
+
 	ob_start();
 
 	$host = "localhost";
@@ -8,6 +9,8 @@
 	$sql_password = "";
 	$db = "kidzcamp";
 	$tbl = "user";
+	$username = $_POST['username'];
+	$password = $_POST['password'];
 
 	mysql_connect($host, $sql_username, $sql_password) or die ("Cannot connect to SQL server.");
 	mysql_select_db("$db") or die ("Cannot select kidzcamp database. (Did you run init.sql yet?)");
@@ -16,10 +19,10 @@
 	mysql_query($query);
 	
 	$search_query = "SELECT * FROM $tbl WHERE username='$username'";
-	$result = mysql_query($query);
-	$user = mysql_fetch_object($result);
+	$result = mysql_query($search_query);
 	
 	session_start();
+	$user = mysql_fetch_object($result);
 	$_SESSION["user"] = $user;
 	header("location: index.php");
 
