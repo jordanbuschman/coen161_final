1262ec39ee6edd736c8fb970cf5c819146167f69
Jordan <jordan.buschman@me.com>
2013-11-26 22:31:51 -0800

    SQL commands run when the input is changed

--- javascript.js
+++ javascript.js
@@ -2,24 +2,28 @@ $(document).ready(function() {
 	var validUser = false;
 	var validPassword = false;
 
-	$('#newUsername').keyup(function() { //Check if desired username is valid or not with ajax
+	$('#newUsername').bind('change keyup input', function() { //Check if desired username is valid or not with ajax
 		var usr = $(this).val();
-		validUser = false;
-    	$.post( "checkForUsername.php", { username:usr })
-  		.done(function( data ) {
-    		if( data == "F" ){
-    			validUser = true;
-    		}
-  		}).fail(function() {
-    		alert( "AJAX FAILED" );
-  		});
+    		$.post( "checkForUsername.php", { username:usr })
+  			.done(function( data ) {
+    				if( data == "F" && usr != "") {
+					validUser = true;
+					$('#newUsername').css('color', 'green');
+				}
+				else {
+					validUser = false;
+					$('#newUsername').css('color', 'red');
+				}
+  			}).fail(function() {
+    				alert( "AJAX FAILED" );
+  			});
   	
-		if (validPassword && validUser){
+		if (validUser && validPassword){
 			$('#create').removeAttr('disabled');
 		}
 		else $('#create').attr('disabled', 'disabled');
 	});
-	$('#newPassword').keyup(function() { //Check if desired password is between 8-12 characters with at least 1 capital letter and 1 number
+	$('#newPassword').bind('change keyup input', function() { //Check if desired password is between 8-12 characters with at least 1 capital letter and 1 number
 		var password = $(this).val();
 		var pattern = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,16}$/; 
 		if (password.match(pattern)) validPassword = true;
