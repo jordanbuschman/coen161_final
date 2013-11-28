92bfd57ac3f1f9a9176258b6ba296be65f1ef6c2
Jordan <jordan.buschman@me.com>
2013-11-28 13:04:39 -0800

    Styling and adding fields for signup

--- javascript.js
+++ javascript.js
@@ -25,24 +25,28 @@ function displaySignup() {
 	data.push("<table style='background-color: orange; padding:0.5em; border-radius: 15px; width: 100%'>",
 		"<tr><td><input type='button' value='Go back' onclick='window.location = window.location.pathname;' /><td></tr>",
 		"<tr>",
-		"<th style='text-align: right'>Username</th>",
+		"<td style='text-align: right'><input name='firstName' type='text' size='16' placeholder='First Name'/></td>",
+		"<td><input name='lastName' type='text' size='16' placeholder='Last Name'/></td>",
+		"</tr>",
+		"<tr>",
+		"<th style='text-align: right' id='usernameText'>Username</th>",
 		"<td><input name='username' type='text' id='newUsername' size='16'/></td>",
 		"</tr>",
 		"<tr>",
-		"<th style='text-align: right'>Password</th>",
-		"<td><input name='password' type='password' id='newPassword' size='16'/></td>",
+		"<th style='text-align: right' id='passwordText'>Password</th>",
+		"<td><input name='password' type='password' id='newPassword' size='16' maxlength='16'/></td>",
 		"</tr>",
 		"<tr>",
-		"<th style='text-align: right'>Confirm Password</th>",
-		"<td><input type='password' id='newPassword2' size='16'/></td>",
+		"<th style='text-align: right' id='password2Text'>Confirm Password</th>",
+		"<td><input type='password' id='newPassword2' size='16' maxlength='16'/></td>",
 		"</tr>",
 		"<tr>",
 		"<th colspan='2' style='text-align: center'><input type='submit' name='Submit' value='Create' id='create' disabled='disabled' /></td>",
 		"<td></td>",
 		"</tr>",
-		"<tr><td colspan='2' id='validUsername' style='text-shadow: 1px 1px black'></td></tr>",
-		"<tr><td colspan='2' id='validPassword' style='text-shadow: 1px 1px black'></td></tr>",
-		"<tr><td colspan='2' id='validPassword2' style='text-shadow: 1px 1px black'></td></tr>",
+		"<tr><td colspan='2' id='validUsername'></td></tr>",
+		"<tr><td colspan='2' id='validPassword'></td></tr>",
+		"<tr><td colspan='2' id='validPassword2'></td></tr>",
 		"</table>");
 
 	$('#background').fadeTo( "slow" , 0.6, function() {
@@ -62,6 +66,7 @@ $(document).ready(function() {
 		if (usr.match(pattern) || usr.trim() == "") {
 			validUser = false;
 			$('#validUsername').html("<span style='color:red'>Username is invalid (No whitespace allowed)</span>");
+			$('#usernameText').css("color", "red");
 			$('#create').attr('disabled', 'disabled');
 			return;
 		}
@@ -70,10 +75,12 @@ $(document).ready(function() {
     				if( data == "F") {
 					validUser = true;
 					$('#validUsername').html("<span style='color:green'>Username is valid</span>");
+					$('#usernameText').css("color", "green");
 				}
 				else {
 					validUser = false;
 					$('#validUsername').html("<span style='color:red'>Username is invalid (Name already taken)</span>");
+					$('#usernameText').css("color", "red");
 				}
   			}).fail(function() {
     				alert( "AJAX FAILED" );
@@ -87,16 +94,19 @@ $(document).ready(function() {
 	$(document).on('change keyup input', '#newPassword', function() { //Check if desired password is between 8-12 characters with at least 1 capital letter and 1 number (and if passwords match)
 		validPassword2 = false;
 		$('#validPassword2').html("<span style='color:red'>Passwords do not match</span>");
+		$('#password2Text').css("color", "red");
 
 		var password = $(this).val();
 		var pattern = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,16}$/; 
 
 		if (password.match(pattern)) {
 			$('#validPassword').html("<span style='color:green'>Password is valid</span>");
+			$('#passwordText').css("color", "green");
 			validPassword = true;
 		}
 		else {
 			$('#validPassword').html("<span style='color:red'>Password must be 8-16 characters long with at least 1 capital letter and 1 number</span>");
+			$('#passwordText').css("color", "red");
 			validPassword = false;
 		}
 
@@ -110,10 +120,12 @@ $(document).ready(function() {
 
 		if (password == $('#newPassword').val()) {
 			$('#validPassword2').html("<span style='color:green'>Passwords match</span>");
+			$('#password2Text').css("color", "green");
 			validPassword2 = true;
 		}
 		else {
 			$('#validPassword2').html("<span style='color:red'>Passwords do not match</span>");
+			$('#password2Text').css("color", "red");
 			validPassword2 = false;
 		}
 
