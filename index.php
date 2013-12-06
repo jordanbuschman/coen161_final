5dec99918fdf3dd70d5eedc7dd673e2ef31fc590
adeartola <adeartola@gmail.com>
2013-12-06 01:09:57 -0800

    distance from top for registration

--- index.php
+++ index.php
@@ -33,16 +33,28 @@
 					}
 				var map = new google.maps.Map(mapCanvas, mapOptions);
 				var infowindow = new google.maps.InfoWindow();
-			var marker, i;
-			for (i = 0; i < locations.length; i++) {  
-				marker = new google.maps.Marker({
-				position: new google.maps.LatLng(locations[i][1], locations[i][2], locations[i][3]),
-				map: map
-			});
+				var marker, i;
+				for (i = 0; i < locations.length; i++) { 
+					infowindow.setContent(locations[i][0]);
+					marker = new google.maps.Marker({
+					position: new google.maps.LatLng(locations[i][1], locations[i][2], locations[i][3]),
+					map: map
+				});
+				google.maps.event.addListener(marker, 'click', (function(marker, i) {
+        return function() {
+          infowindow.setContent(locations[i][0]);
+		  infowindow.open(map, marker);
+		}
+		}
+		) (marker, i));
+				
 			}
+				
+				google.maps.event.addListener(marker, 'click', function() {
+					infowindow.open(map,marker);
+				});
 			}
 			google.maps.event.addDomListener(window, 'load', initialize);
-			
 			$(document).ready(function() {
 				$.post("getCartCount.php", {userId: <?php echo (isset($_SESSION['user']) ? $_SESSION['user']->id : -1); ?>})
 				.done(function(data) {
--- maplocations.js
+++ maplocations.js
@@ -1,9 +1,9 @@
 var locations = [
-      ['Stanford', 37.4300, -122.1700, 4],
-      ['University of San Francisco', 37.7749, -122.4525, 5],
-      ['CalTech', 34.1386, -118.1255, 3],
-      ['University of Southern California', 34.0205, -118.2856, 2],
-      ['University of California: Los Angles', 34.064, -118.4421, 1],
-	  ['Santa Clara University', 37.3492, -121.9381, 6],
-      ['Loyola Marymount University', 33.9700, -118.4180, 7],
+      ['<html><div id="maps">Stanford University<br />Session 2</div></html>', 37.4300, -122.1700, 4],
+      ['<html><div id="maps">University of San Francisco<br />Session 4</div></html>', 37.7749, -122.4525, 5],
+      ['<html><div id="maps">California Institute of Technology<br />Session 7</div></html>', 34.1386, -118.1255, 3],
+      ['<html><div id="maps">University of Southern California<br />Session 5</div></html>', 34.0205, -118.2856, 2],
+      ['<html><div id="maps">University of California: Los Angles<br />Session 6</div></html>', 34.064, -118.4421, 1],
+	  ['<html><div id="maps">Santa Clara University<br />Session 1</div></html>', 37.3492, -121.9381, 6],
+      ['<html><div id="maps">Loyola Marymount University<br />Session 3</div></html>', 33.9700, -118.4180, 7],
     ];
