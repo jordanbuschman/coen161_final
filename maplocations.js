755f1a1e67e75fa33600e556da3ea12c7745d691
adeartola <adeartola@gmail.com>
2013-12-05 22:35:50 -0800

    added map locations

---
+++ maplocations.js
@@ -0,0 +1,9 @@
+var locations = [
+      ['Stanford', 37.4300, -122.1700, 4],
+      ['University of San Francisco', 37.7749, -122.4525, 5],
+      ['CalTech', 34.1386, -118.1255, 3],
+      ['University of Southern California', 34.0205, -118.2856, 2],
+      ['University of California: Los Angles', 34.064, -118.4421, 1],
+	  ['Santa Clara University', 37.3492, -121.9381, 6],
+      ['Loyola Marymount University', 33.9700, -118.4180, 7],
+    ];
